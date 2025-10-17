c32e08fbea7f89a4aa8c5b5191ac00807cdc6b35
Kavya Konisa <konisakavya2411@gmail.com>
2025-10-17 09:05:34 -0400

    Added Complete code for Kit

---
+++ src/com/command/homeautomation/RemoteLoaderKit.java
@@ -0,0 +1,104 @@
+package com.command.homeautomation;
+
+public class RemoteLoaderKit {
+	
+	public static void main(String[] args) {
+		RemoteControl remoteControl = new RemoteControl();
+		Light livingRoomLight = new Light("Living Room");
+		Light kitchenRoomLight = new Light("Kitchen");
+		CeilingFan ceilingFan = new CeilingFan("LIving Room");
+		GarageDoor garageDoor = new GarageDoor("Garage");
+		Stereo stereo = new Stereo("Living Room");
+		Television television = new Television("Living Room");
+
+		LightOnCommand livingRoomLightOn = new LightOnCommand(livingRoomLight);
+		LightOffCommand livingRoomLightOff = new LightOffCommand(livingRoomLight);
+
+		LightOnCommand kitchenRoomLightOn = new LightOnCommand(kitchenRoomLight);
+		LightOffCommand kitchenRoomLightOff = new LightOffCommand(kitchenRoomLight);
+
+		CeilingFanOnCommand ceilingFanOn = new CeilingFanOnCommand(ceilingFan);
+		CeilingFanMediumCommand ceilingFanMedium = new CeilingFanMediumCommand(ceilingFan);
+		CeilingFanLowCommand ceilingFanLow = new CeilingFanLowCommand(ceilingFan);
+		CeilingFanOffCommand ceilingFanOff = new CeilingFanOffCommand(ceilingFan);
+
+		GarageDoorUpCommand garageDoorUp = new GarageDoorUpCommand(garageDoor);
+		GarageDoorDownCommand garageDoorDown = new GarageDoorDownCommand(garageDoor);
+
+		StereoOnWithCDCommand stereoOnWithCD = new StereoOnWithCDCommand(stereo);
+		StereoOffCommand stereoOff = new StereoOffCommand(stereo);
+	
+		TelevisionOnCommand televisionOn = new TelevisionOnCommand(television);
+		TelevisionOffCommand televisionOff = new TelevisionOffCommand(television);
+		
+		Command[] partyOn = {livingRoomLightOn,stereoOnWithCD,televisionOn};
+		Command[] partyOff = {livingRoomLightOff,stereoOff ,televisionOff};
+		
+		MacroCommand partyOnMacro = new MacroCommand(partyOn);
+		MacroCommand partyOffMacro = new MacroCommand(partyOff);
+		
+		remoteControl.setCommand(0, livingRoomLightOn, livingRoomLightOff);
+		remoteControl.setCommand(1, kitchenRoomLightOn, kitchenRoomLightOff);
+		remoteControl.setCommand(2, garageDoorUp, garageDoorDown);
+		remoteControl.setCommand(3, stereoOnWithCD, stereoOff);
+		
+		remoteControl.setCommand(4, ceilingFanOn, ceilingFanOff);
+		remoteControl.setCommand(5, ceilingFanMedium, ceilingFanOff);
+
+		remoteControl.setCommand(6, partyOnMacro, partyOffMacro);
+		
+		
+
+		
+		remoteControl.onButtonWasPushed(0);
+		remoteControl.offButtonWasPushed(0);
+		System.out.println(remoteControl);
+		remoteControl.undoButtonWasPushed();
+		
+		
+		remoteControl.onButtonWasPushed(1);
+		remoteControl.offButtonWasPushed(1);
+		System.out.println(remoteControl);
+		remoteControl.undoButtonWasPushed();
+		
+		remoteControl.offButtonWasPushed(1);
+		System.out.println(remoteControl);
+		remoteControl.undoButtonWasPushed();
+		System.out.println("--------ceiling Fan 1-------------------");
+
+		
+		remoteControl.onButtonWasPushed(4);
+		remoteControl.offButtonWasPushed(4);
+		System.out.println(remoteControl);
+		remoteControl.undoButtonWasPushed();
+		
+		System.out.println("--------ceiling Fan 2-------------------");
+
+		remoteControl.onButtonWasPushed(5);
+		remoteControl.offButtonWasPushed(5);
+		System.out.println(remoteControl);
+		remoteControl.undoButtonWasPushed();
+		
+		System.out.println("--------ceiling Fan3-------------------");
+
+		remoteControl.onButtonWasPushed(6);
+		System.out.println(remoteControl);
+		remoteControl.undoButtonWasPushed();
+		
+		
+		System.out.println(remoteControl);
+		System.out.println("Pushing Macro On------------");
+
+		remoteControl.onButtonWasPushed(6);
+		System.out.println("Pushing Macro Off------------");
+
+		remoteControl.offButtonWasPushed(6);
+		System.out.println("Pushing Macro Undo------------");
+		remoteControl.undoButtonWasPushed();
+		
+
+		
+		
+	}
+
+}
