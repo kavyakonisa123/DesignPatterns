9a2a295bc42635e7f34290d7985bed2bbd64e407
Kavya Konisa <konisakavya2411@gmail.com>
2025-10-27 17:45:35 -0400

    Firstattempt on MenuMerge

---
+++ src/com/interator/housemerge/Waitress.java
@@ -0,0 +1,36 @@
+package com.interator.housemerge;
+
+import java.util.List;
+
+public class Waitress {
+	public static void main(String args[]) {
+		
+		PancakeHouseMenu pancakeHouseMenu= new PancakeHouseMenu();
+		List<MenuItem> breakfastMenu= pancakeHouseMenu.getMenuItems();
+		
+		System.out.println("\n Breakfast.......................");
+
+		
+		for(int i=0; i< breakfastMenu.size();i++) {
+			MenuItem menuItem = breakfastMenu.get(i);
+			System.out.print(menuItem.getName()+ "---- ");
+			System.out.print(menuItem.getDescription()+ "---- ");
+			System.out.print(menuItem.getPrice()+ " "+"\n");
+	
+		}
+		System.out.println("\n Lunch.......................");
+
+		DinerMenu dinerMenu= new DinerMenu();	
+		MenuItem[] lunchItems= dinerMenu.getMenuItems();
+		for(int i=0; i< lunchItems.length;i++) {
+			MenuItem menuItem = lunchItems[i];
+			System.out.print(menuItem.getName()+ "---- ");
+			System.out.print(menuItem.getDescription()+ "---- ");
+			System.out.print(menuItem.getPrice()+ " "+"\n");
+	
+		}
+		
+		
+	}
+
+}
